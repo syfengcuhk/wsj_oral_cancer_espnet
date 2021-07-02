bba685bb6e19078ad284ff0e943b51bc2320e807
syfengcuhk <fengsym.ee@gmail.com>
2021-07-02 14:20:11 +0200

    2nd commit

---
+++ asr1/scripts_hpc/sbatch/wsj_oc_rnnlm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#you can control the resources and scheduling with '#SBATCH' settings
+# (see 'man sbatch' for more information on setting these parameters)
+# The default partition is the 'general' partition
+#SBATCH --partition=general
+# The default Quality of Service is the 'short' QoS (maximum run time: 4 hours)
+#SBATCH --qos=long
+# The default run (wall-clock) time is 1 minute
+#SBATCH --time=28:00:00
+# The default number of parallel tasks per job is 1
+#SBATCH --ntasks=1
+# Request 1 CPU per active thread of your program (assume 1 unless you specifically set this)
+# The default number of CPUs per task is 1 (note: CPUs are always allocated per 2)
+#SBATCH --cpus-per-task=4
+# The default memory per node is 1024 megabytes (1GB) (for multiple tasks, specify --mem-per-cpu instead)
+#SBATCH --mem=12G
+#SBATCH --gres=gpu:2
+# Set mail type to 'END' to receive a mail when the job finishes
+# Do not enable mails when submitting large numbers (>20) of jobs at once
+#SBATCH --mail-type=END
+##SBATCH --nodelist=ewi1
+
+#srun bash run.sh --stage 3 --stop-stage 4 --ngpu 2  --use-wordlm true  # 13 hours
+srun bash run.sh --stage 3 --stop-stage 4 --ngpu 2  --use-wordlm false # 25 hours
+
